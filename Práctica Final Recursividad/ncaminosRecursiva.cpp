33528cc06557b958023011dc37c75a7c68240da5
Juanfra <juan.tm10@hotmail.com>
2019-05-28 12:17:36 +0200

    Ncaminos Recursiva funcionando

--- Práctica Final Recursividad/ncaminosRecursiva.cpp
+++ Práctica Final Recursividad/ncaminosRecursiva.cpp
@@ -2,14 +2,16 @@
 
 using namespace std;
 
-#define N 5
+//#define N 6
 
-int **crearLaberinto(int);
-void pintarLaberinto(int**);
-void imprimirLaberinto(int**);
-void funcionLaberinto(int**,int*,int*,int*,int,int,int,int, int,int*);
+int introducirValor(string);
+int introducirTamanyo(string);
+int **crearLaberinto(int,int);
+void pintarLaberinto(int**,int,int);
+void imprimirLaberinto(int**,int,int);
+void funcionLaberinto(int**,int*,int*,int*,int,int,int,int, int,int*,int,int);
 void siguienteCandidato(int*, int*, int*, int, int, int);
-bool factible(int**,int,int,int,int);
+bool factible(int**,int,int,int,int,int,int);
 
 
 
@@ -17,77 +19,133 @@ int main()
 {
     int **laberinto;
     int *X,*Y,*M,*sol;
-    int k,xentrada,yentrada,xsalida,ysalida;
+    int k,xentrada,yentrada,xsalida,ysalida,F,C;
+    string valor1  = "entrada X";
+    string valor2  = "entrada Y";
+    string valor3  = "salida X";
+    string valor4  = "salida Y";
+    string fila    = " fila N";
+    string columna = " columna M";
 
-    X = new int[N*N];
-    Y = new int[N*N];
-    M = new int[N*N];
-    sol = new int[N*N];
-    int ntotal = 0;
 
-    xentrada = 1;
-    yentrada = 1;
-    xsalida  = 3;
-    ysalida  = 2;
+    F = introducirTamanyo(fila);
+    C = introducirTamanyo(columna);
+
+    X = new int[F*C];
+    Y = new int[F*C];
+    M = new int[F*C];
+    sol = new int[F*C];
+
+    //COORDENADAS DE ENTRADA
+    do{
+        cout<<"\nEl valor a introducir debe de estar entre 0 y "<<F<<endl;
+        xentrada = introducirValor(valor1);
+    }while(xentrada > F);
+    do{
+        cout<<"\nEl valor a introducir debe de estar entre 0 y "<<C<<endl;
+        yentrada = introducirValor(valor2);
+    }while(yentrada > C);
+
+    //COORDENADAS DE SALIDA
+    do{
+        cout<<"\nEl valor a introducir debe de estar entre "<<xentrada<<" y "<<F<<endl;
+        xsalida = introducirValor(valor3);
+        if(xsalida > F){
+            xsalida = 0;
+        }
+    }while(xentrada >= xsalida);
+
+     do{
+        cout<<"\nEl valor a introducir debe de estar entre "<<yentrada<<" y "<<C<<endl;
+        ysalida = introducirValor(valor4);
+        if(ysalida > C){
+            ysalida = 0;
+        }
+    }while(yentrada >= ysalida);
+
+    cout<<"Punto de entrada: ("<<xentrada<<","<<yentrada<<")"<<endl;
+    cout<<"Punto de salida: ("<<xsalida<<","<<ysalida<<")"<<endl;
 
     k=1;
     X[k] = xentrada;
     Y[k] = yentrada;
 
+    laberinto = crearLaberinto(F,C);
+    pintarLaberinto(laberinto,F,C);
+  //  imprimirLaberinto(laberinto,F,C);
 
-
-    laberinto = crearLaberinto(N);
-    pintarLaberinto(laberinto);
-    imprimirLaberinto(laberinto);
-
-
-
-    funcionLaberinto(laberinto,X,Y,M,xentrada,yentrada,xsalida,ysalida,k,sol);
-
-    for(int j=1;j<N*N;j++){
-        if(sol[j]==1){
-            cout<<"sol "<<endl;
-            ntotal += sol[j];
-        }
+    for(int r=0;r<F*C;r++){
+        sol[r]=0;
     }
 
+    int total = 0;
+    funcionLaberinto(laberinto,X,Y,M,xentrada,yentrada,xsalida,ysalida,k,sol,F,C);
+
+
+    /*
+    for(int j=1;j<F*C;j++){
+        if(sol[j] <=N && sol[j] != 0){
+            ntotal+=1;
+        }
+        cout<<"valor de sol["<<j<<"] ="<<sol[j]<<endl;
+    }*/
+
     cout<<endl;
-    cout<<"El total de caminos encontrados es de: "<<ntotal<<endl;
+    cout<<"El total de caminos encontrados es de: "<<total<<endl;
     cout<<endl;
-    imprimirLaberinto(laberinto);
+    imprimirLaberinto(laberinto,F,C);
 
     //LIBERAR MEMORIA
-    for(int i=0;i<N;i++)delete[] laberinto[i];
+    for(int i=0;i<F;i++)delete[] laberinto[i];
     delete[] X;
     delete[] Y;
     delete[] M;
+    delete[] sol;
     return 0;
-}
+}//fin main
 
 //CREAR LABERINTO y DARLE VALORES
-int **crearLaberinto(int n){
+int **crearLaberinto(int f, int c){
     int **maze;
-    maze = new int*[n];
-    for(int x=0;x<n;x++){
-        maze[x] = new int[n];
+    maze = new int*[f];
+    for(int x=0;x<f;x++){
+        maze[x] = new int[c];
     }
 
     return maze;
 }
 
-
-void pintarLaberinto(int **maze){
-    for(int i=0;i<N;i++){
-        for(int j=0;j<N;j++){
+//DAR VALORES AL LABERINTO
+void pintarLaberinto(int **maze, int f, int c){
+    for(int i=0;i<f;i++){
+        for(int j=0;j<c;j++){
             maze[i][j] = 0;
         }
     }
 }
 
+//INTRODUCIR VALORES DE LAS COORDENADAS
+int introducirValor(string valor){
+    int num;
+    cout<<"Introduce el valor de "<<valor<<":";
+    cin>>num;
+
+    return num;
+}
+
+
+int introducirTamanyo(string valor){
+    int num;
+    cout<<"Introduce el valor de "<<valor<<":";
+    cin>>num;
+
+    return num;
+}
+
 //IMPRIMIR LABERINTO
-void imprimirLaberinto(int **maze){
-    for(int i=0;i<N;i++){
-        for(int j=0;j<N;j++){
+void imprimirLaberinto(int **maze,int f, int c){
+    for(int i=0;i<f;i++){
+        for(int j=0;j<c;j++){
             cout<<maze[i][j]<<" ";
         }
         cout<<endl;
@@ -95,51 +153,34 @@ void imprimirLaberinto(int **maze){
 }
 
 
+
 //FUNCIÓN LABERINTO
-void funcionLaberinto(int **maze,int *Y, int *X,int *M, int xentrada, int yentrada, int xsalida, int ysalida, int k, int *sol)
+void funcionLaberinto(int **maze,int *Y, int *X,int *M, int xentrada, int yentrada, int xsalida, int ysalida, int k, int *sol,int f,int c)
 {
     int xsig,ysig,xk,yk;
     M[k]=0;
-    maze[xentrada][yentrada]=1;
-    maze[xsalida][ysalida]=2;
-
-    while(M[k]<3){
-
-  /*  for(int i=0;i<3;i++){
-        M[k] = i;
-        cout<<"Vector M:"<<i<<" ";
-        for(int c=1;c<=k;c++){
-            cout<<" (";
-            cout<<"X"<<X[k]<<" Y"<<Y[k];
-            cout<<") ";
-        }
-        cout<<endl;*/
-
-
+    while(M[k]<3)
+    {
         M[k] = M[k] + 1;
         siguienteCandidato(X,Y,M,k,xsalida,ysalida);
         xsig = X[k];
         ysig = Y[k];
-        if(factible(maze,xsig,ysig,xsalida,ysalida)==true){
+        if(factible(maze,xsig,ysig,xsalida,ysalida,f,c)==true){
             X[k+1] = xsig;
             Y[k+1] = ysig;
+            cout<<"Nivel k: "<<k<<" ("<<X[k+1]<<","<<Y[k+1]<<")"<<endl;
             if(X[k+1] == xsalida && Y[k+1] == ysalida){
-                sol[k] = 1;
+                sol[k] = k;
                 cout<<"Se ha encontrado la salida numero: "<<sol[k]<<endl;
                 maze[xsalida][ysalida]=1;
 
-               // X[k-1] = xsig;
-               // Y[k-1] = ysig;
-              //  imprimirLaberinto(maze);
-             //   pintarLaberinto(maze);
             }else{
                 xk = X[k];
                 yk = Y[k];
                 maze[xk][yk] = 1;
-                funcionLaberinto(maze,Y,X,M,xentrada,yentrada,xsalida,ysalida,k+1,sol);
+                funcionLaberinto(maze,Y,X,M,xentrada,yentrada,xsalida,ysalida,k+1,sol,c,f);
             }
         }//fin factible
-   // }//for
     }//fin while
 }
 
@@ -148,38 +189,30 @@ void siguienteCandidato(int *X, int *Y, int *M, int k, int xsalida, int ysalida)
 {
     if(M[k]==1){
         Y[k] = Y[k] + 1;
-        //cout<<"derecha "<<endl;
-        //cout<<"Valor de Y[k] "<<Y[k]<<" ||Valor de ysalida]: "<<ysalida<<endl;
     }
     if(M[k]==2){
         Y[k] = Y[k] - 1;
         X[k] = X[k] + 1;
-       // cout<<"abajo"<<endl;
     }
     if(M[k]==3){
         Y[k] = Y[k] + 1;
-       // cout<<"diagonal"<<endl;
     }
 }
 
 
 //función factible
-bool factible(int **maze, int xsig, int ysig, int xsalida, int ysalida){
+bool factible(int **maze, int xsig, int ysig, int xsalida, int ysalida, int f, int c){
 
-  if(xsig > N-1 || ysig > N-1){
+  if(xsig > f-1 || ysig > c-1){
         return false;
 
     }else{
         if( xsig > xsalida ) {
-            //cout<<"\nEntra en funcion factible  de Xsig devuelve false!!!!! "<<endl;
             return false;
         }else if(ysig > ysalida ){
-		//	  cout<<"\nEntra en funcion factible de ysig devuelve false!!!!! "<<endl;
             return false;
 		}
 		else{
-         //   cout<<"\nEntra en funcion factible devuelve true!!!!! "<<endl;
-       //     cout<<"Valor de xsig "<<xsig<<" ||Valor de ysig: "<<ysig<<endl;
             return true;
         }
     }
